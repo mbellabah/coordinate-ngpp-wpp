a7d2aa4a7e2e684561976477a3c6c04856a808f8
Bella Bah <bellabah@mit.edu>
2018-11-18 22:27:43 -0500

    Made code compatible with ISONE Webservice API

--- pricing_nodes.py
+++ pricing_nodes.py
@@ -1,14 +1,19 @@
-import numpy as np
-import scipy
-import matplotlib.pyplot as plt
+'''
+Substantial credit for the following code goes to WattTime group
+whose code I have adapted to fit the needs of the academic project.
+https://github.com/WattTime/pyiso Version 2.17
+'''
+
+import sys
 import requests
+import numpy as np
 import pandas as pd
+import datetime
+import pytz
 from requests.auth import HTTPBasicAuth
+from typing import Type
+from collections import defaultdict
 
-username = 'bellabah@mit.edu'
-password = 'bella123'
-
-auth = HTTPBasicAuth(username, password)
 
 # TODO: Find the pricing nodes history between given date
 # TODO: Fix up confusion regarding the LD, the labels used
@@ -16,27 +21,18 @@ auth = HTTPBasicAuth(username, password)
 
 class ISONEClient:
     NAME = 'ISONE'
-
     base_url = 'https://webservices.iso-ne.com/api/v1.1'
+    TZ_NAME = 'America/New_York'
 
-    locations = {
-        'INTERNALHUB': 4000,
-        'MAINE': 4001,
-        'NEWHAMPSHIRE': 4002,
-        'VERMONT': 4003,
-        'CONNECTICUT': 4004,
-        'RHODEISLAND': 4005,
-        'SEMASS': 4006,
-        'WCMASS': 4007,
-        'NEMASSBOST': 4008,
-    }
-
-    def __init__(self, auth):
+    def __init__(self, username, password):
         try:
-            self.auth = auth
+            self.auth = HTTPBasicAuth(username, password)
+
         except KeyError:
             msg = 'Provide authentication'
             raise RuntimeError(msg)
+
+        self.options = {}
 
     def fetch_data(self, endpoint):
         url = self.base_url + endpoint
@@ -44,60 +40,96 @@ class ISONEClient:
 
         if response:
             return response.json()
-        else: return {}
+        else:
+            return {}
 
-    def parse_json_lmp_data(self, data):
+    def parse_lmp_data(self, data: Type['json']):
         try:
-            return data['HourlyLmps']['HourlyLmp']
+            return data['FiveMinLmps']['FiveMinLmp']
+
         except (KeyError, TypeError):
-            msg = f'Could not parse ISONE lmp data: {data}'
-            raise ValueError(msg)
+            raise ValueError(f'Failed to parse {data}')
 
-    def _parse_lmp(self, json):
+    def raw_lmp_data_to_pd(self, raw_data) -> pd.DataFrame:
+        processed_dict = defaultdict(list)
 
-        raise NotImplementedError
+        def extract_data(five_min_data: dict) -> dict:
+            processed_dict['Date'].append(five_min_data['BeginDate'][:10])
+            processed_dict['Minute Ending'].append(five_min_data['BeginDate'][14:16])
+            processed_dict['Location ID'].append(five_min_data['Location']['@LocId'])
+            processed_dict['Location Name'].append(five_min_data['Location']['$'])
+            processed_dict['Locational Marginal Price'].append(five_min_data['LmpTotal'])
+            processed_dict['Energy Component'].append(five_min_data['EnergyComponent'])
+            processed_dict['Congestion Component'].append(five_min_data['CongestionComponent'])
+            processed_dict['Marginal Loss Component'].append(five_min_data['LossComponent'])
 
-        dataframe = pd.DataFrame(json)
+        for day_dp in raw_data:
+            for five_min in day_dp:
+                extract_data(five_min)
+
+        dataframe = pd.DataFrame.from_dict(processed_dict)
         return dataframe
 
-    def get_lmp(self, node_id='INTERNALHUB', **kwargs):
-        try:
-            location_id = self.locations[node_id.upper()]
-        except KeyError:
-            msg = 'No LMP data available'
+    def get_lmp(self, node_id: int, **kwargs):
+        self.options.update(kwargs)
+        raw_data = []
+
+        for endpoint in self.request_endpoints(node_id):
+            data = self.fetch_data(endpoint)
+            raw_data.append(self.parse_lmp_data(data))
+
+        return self.raw_lmp_data_to_pd(raw_data)    # convert to pd df
+
+    def request_endpoints(self, location_id=None):
+        ext = ''
+        if self.options['data'] == 'lmp' and location_id is not None:
+            base_endpoint = 'fiveminutelmp'
+            ext = f'/location/{location_id}'
+
+        request_endpoints = []
+        if self.options['latest']:
+            request_endpoints.append(f'/{base_endpoint}/current{ext}.json')
+
+        elif self.options['start_at'] and self.options['end_at']:
+            for date in self.dates():
+                date_str = date.strftime('%Y%m%d')
+                request_endpoints.append(f'/{base_endpoint}/day/{date_str}{ext}.json')
+
+        else:
+            msg = 'Either latest or start_at and end_at must be both provided'
             raise ValueError(msg)
 
-        raise NotImplementedError
+        return request_endpoints
 
-    def compute_data_details(self, lmp_1_id: str, lmp_2_id: str) -> tuple:
+    def dates(self):
+        dates = []
 
-        lmp_1 = self.get_lmp(node_id=lmp_1_id)
-        lmp_2 = self.get_lmp(node_id=lmp_2_id)
+        if self.options['start_at'] and self.options['end_at']:
+            local_start = self.options['start_at'].astimezone(pytz.timezone(self.TZ_NAME))
+            local_end = self.options['end_at'].astimezone(pytz.timezone(self.TZ_NAME))
+            this_date = local_start.date()
+            while this_date <= local_end.date():
+                dates.append(this_date)
+                this_date += datetime.timedelta(days=1)
 
-        # convert into numpy array
-        # get the difference between the two
-        # compute statistical information
-        # return correlation measure
-
-        raise NotImplementedError
-
-
-def client_main():
-    Client = ISONEClient(auth)
-
-    location_id = 'LD.COLBURN 13.8'
-    # ext = '/location/%s' % location_id
-    ext = ''
-    base_endpoint = 'hourlylmp/da/final'
-    endpoint = '/%s/current%s.json' % (base_endpoint, ext)
-
-    # print(Client.parse_json_lmp_data(Client.fetch_data(endpoint)))
-    print(Client.fetch_data(endpoint))
+        return dates
 
 
 
+def isone_main():
+    ISONE = ISONEClient(username=sys.argv[1], password=sys.argv[2])
+
+    node_id = 355
+
+    today = datetime.datetime.today()
+    start_at = today - datetime.timedelta(days=2)
+    end_at = today - datetime.timedelta(days=1)
+
+    msg = ISONE.get_lmp(node_id=node_id, data='lmp', latest=False, start_at=start_at, end_at=end_at)
+
+    print(msg)
 
 
 if __name__ == '__main__':
-    client_main()
+    isone_main()
 
